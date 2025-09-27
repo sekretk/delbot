90ebddeca919cfc52c98fff68432a81171fdc5a4
Konstantin Sekret <ksekret@devexperts.com>
2025-09-27 18:39:32 +0300

    change to trpc

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { AppDataDto } from '@delbot/shared';
+import { AppDataDto, HealthResponseDto } from '@delbot/shared';
 
 @Injectable()
 export class AppService {
   getData(): AppDataDto {
     return { message: 'Hello API' };
+  }
+
+  getHealth(): HealthResponseDto {
+    return {
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
